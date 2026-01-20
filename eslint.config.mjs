1cf79a4f61dbdc5eb7cb41421ff43ae4aa52cad6
yuelinghuashu <1263476185@qq.com>
2026-01-20 21:40:10 +0800

    first commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,4 @@
 // @ts-check
-import withNuxt from './node_modules/.cache/nuxt/.nuxt/eslint.config.mjs'
-
-export default withNuxt(
-  // Your custom configs here
-)
+import withNuxt from "./.nuxt/eslint.config.mjs";
+export default withNuxt();
+// Your custom configs here
